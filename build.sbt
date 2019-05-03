17d3973e0f5c8ad49186cf384c628b5c6de3f93a
Bidur_Banstola <bidur.banstola@gmail.com>
2019-05-03 12:35:02 +0545

    Revisions

--- build.sbt
+++ build.sbt
@@ -4,13 +4,15 @@ version := "1.0"
 
 scalaVersion := "2.12.8"
 
-
 libraryDependencies += "org.scalactic" %% "scalactic" % "3.0.5"
 
 libraryDependencies += "org.scalatest" %% "scalatest" % "3.0.5" % "test"
 
 // https://mvnrepository.com/artifact/junit/junit
 libraryDependencies += "junit" % "junit" % "4.12" % Test
+
+// PureConfig
+libraryDependencies += "com.github.pureconfig" %% "pureconfig" % "0.10.2"
 
 //Typesafe
 libraryDependencies += "com.typesafe" % "config" % "1.3.0"
--- src/main/scala/Classify/Main.scala
+++ src/main/scala/Classify/Main.scala
@@ -1,42 +1,46 @@
 package Classify
 
-import com.typesafe.config._
 import javax.imageio.ImageIO
 import Classify.Classifier._
+import pureconfig.error.ConfigReaderFailures
+import pureconfig.loadConfig
+import pureconfig.generic.auto._
 
 object Main {
 
-  def main(args: Array[String]): Unit = {                           // main function
+  def main(args: Array[String]): Unit = {                                      // main function
 
-    val config = ConfigFactory.load()
+    //val config = ConfigFactory.load()
 
-    val destinationLocation = config.getString("image.destination")      // Output Directory ; set new src/main/resources/from application.conf
+    val simpleConfig: Either[ConfigReaderFailures, Image] = loadConfig[Image]
 
-    val files = getListOfFiles(config.getString("image.source"))         // getting files from the source directory
+    simpleConfig match {
+      case Left(ex) => ex.toList.foreach(println)
 
-    //val valid = List("jpg" ,"png")                 // List of file extensions for validating   ; add new values as per your requirements
+      case Right(config) =>
 
-    for (file <- files) {                                           // traversing each files from the source location
+        val destinationLocation = config.image.destination // Output Directory ; set new src/main/resources/from application.conf
 
-      //if (valid contains file.getName.split('.')(1).toString) {     // checking if the file's extension is present in the list valid or not
+        val files = getListOfFiles(config.image.source) // getting files from the source directory
 
-//      if (file.getName.endsWith(".png") || file.getName.endsWith(".jpg")){
+        for (file <- files) {
+          // traversing each files from the source location
 
-        val photo1 = ImageIO.read(file)                             // reading each file
+          val photo1 = ImageIO.read(file) // reading each file
 
-        val photo2 = makeGray(photo1)                               // grayscale conversion stored
+          val photo2 = makeGray(photo1) // grayscale conversion stored
 
-        if (calculate(photo2)<config.getInt("image.threshold")) {                                 // grayscaled image passed to calculate function
+          if (calculate(photo2) < config.image.threshold) {
+            // grayscaled image passed to calculate function
 
-          // if  avg <= threshold ; writing image in the destination folder by name_dark_avg
-          ImageIO.write(ImageIO.read(file), "jpg", new java.io.File(destinationLocation + file.getName.take(file.getName.lastIndexOf('.')) + "_dark_" + calculate(photo2) + ".jpg"))
-        } else {
-          // if avg > threshold ; writing image in the destination folder by name_bright_avg
-          ImageIO.write(ImageIO.read(file), "jpg", new java.io.File(destinationLocation + file.getName.take(file.getName.lastIndexOf('.')) + "_bright_" + calculate(photo2) + ".jpg"))
+            // if  avg <= threshold ; writing image in the destination folder by name_dark_avg
+            ImageIO.write(ImageIO.read(file), "jpg", new java.io.File(destinationLocation + file.getName.take(file.getName.lastIndexOf('.')) + "_dark_" + calculate(photo2) + ".jpg"))
+          } else {
+            // if avg > threshold ; writing image in the destination folder by name_bright_avg
+            ImageIO.write(ImageIO.read(file), "jpg", new java.io.File(destinationLocation + file.getName.take(file.getName.lastIndexOf('.')) + "_bright_" + calculate(photo2) + ".jpg"))
+          }
         }
-
-      //else println(file.getName + " is not of an image file.")    // In case the extension validation fails, console returns the print statement
+      }
     }
-  }
 
 }
